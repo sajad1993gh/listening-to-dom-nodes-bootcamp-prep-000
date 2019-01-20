ccdf72e6a1b6486e437f918c15a6545b545cbcd3
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-20 19:24:10 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -49,5 +49,19 @@ for (let i = 0; i < divs.length; i++) {
 }
 
 
+const divs = document.querySelectorAll('div');
+ 
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation();
+ 
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
+
+
 
 */
