711fb88f9f1d88d6feafc0c8e71580872c9f63ea
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-20 19:11:51 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.getElementById('main');
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
+const input = document.querySelector('input')
+ 
+input.addEventListener('keydown', function(e) {
+  if (e.key === "g") {
+    return e.preventDefault()
+  } 
+  else if(e.key === "h"){
+    return e.stopPropagation()
+  }
+  else {
+    console.log(e.key)
+  }
+});
